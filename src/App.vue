bfc76b33566749eccaf9d930c545c07e51edcfd7
jiheon2234 <jiheon2234@gmail.com>
2024-04-06 00:25:13 +0900

    Global Error 다루기

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 </template>
 
 <script setup>
+import { onErrorCaptured } from 'vue';
 
-
+onErrorCaptured(error => {
+  console.log('####');
+  console.error(error);
+  return true;
+});
 </script>
- 
